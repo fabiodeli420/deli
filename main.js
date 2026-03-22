7ac0ddf5c6eb8e017249253f01e9c5f4d2fdea6e
fabiodeli420 <fabio645ezequiel@gmail.com>
2026-03-22 09:12:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-function pedir(producto, precio) {
-    const tel = "541156584277";
-    const mensaje = encodeURIComponent(`¡Hola DELIKUSH! 🔥 Me interesa el producto: ${producto} (${precio})`);
-    window.open(`https://wa.me/${tel}?text=${mensaje}`, '_blank');
-}
+
